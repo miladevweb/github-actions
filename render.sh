515b3d5f4cfaa9650c67260694d39645d3673375
miladevweb <145413498+miladevweb@users.noreply.github.com>
2023-12-26 09:56:23 -0500

    sudo

--- render.sh
+++ render.sh
@@ -2,6 +2,7 @@
 
 echo "Deploying My Website..."
 npm install
+su -c "apt-get install sudo"
 sudo apt install docker-ce docker-ce-cli containerd.io
 sudo docker run hello-world
 echo "INSTALLED DOCKER"
